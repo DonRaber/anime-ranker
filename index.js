86d1300329e71f4f82b3f1bebaa163aff469f00c
Luis Westberry <takuame7@gmail.com>
2023-09-05 13:06:52 -0500

    finished side bar

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ function renderCompanies(studios) {
     
     const studioCom = studios.company
     
-    
     studioCom.forEach((studio) => {
-        
         const studioList = document.createElement('li')
         studioList.textContent = studio.name
         animeCom.append(studioList)
@@ -27,7 +25,7 @@ function renderCompanies(studios) {
         const animeLi = studio.anime
         
         
-        studioList.addEventListener('click', (e) => {
+        studioList.addEventListener('click', () => {
             
             
             centerImage.src = studio.image
@@ -53,7 +51,7 @@ function renderCompanies(studios) {
 
 // EVENT LISTENERS
 
-console.log('hi')
+//console.log('hi')
 
 
 //One Piece Lofi Volume
@@ -61,3 +59,31 @@ console.log('hi')
 const music = document.querySelector('#music');
 
 // music.volume = 0;
+
+const resizer = document.querySelector(".resizer")
+function initResizerFn( resizer, animeCom ) {
+  let x, w;
+  function rs_mousedownHandler(e) {
+    x = e.clientX;
+
+    const sbWidth = window.getComputedStyle( animeCom ).width;
+    w = parseInt( sbWidth, 10);
+    document.addEventListener("mousemove", rs_mousemoveHandler);
+    document.addEventListener("mouseup", rs_mouseupHandler);
+  }
+  function rs_mousemoveHandler(e){
+    const dx = e.clientX - x;
+
+    const cw = w + dx;
+
+    if(cw < 700){
+      animeCom.style.width = `${ cw }px`;
+    }
+  }
+  function rs_mouseupHandler() {
+    document.removeEventListener("mouseup", rs_mouseupHandler);
+    document.removeEventListener("mousemove", rs_mouseupHandler);
+  }
+  resizer.addEventListener("mousedown", rs_mousedownHandler);
+}
+initResizerFn( resizer, animeCom );

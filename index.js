fb12bf5ff8188526b6590eecbf7f0209914a449c
Donovan Raber <donovan.raber@outlook.com>
2023-09-04 17:01:05 -0700

    JS Vers. 1

--- index.js
+++ index.js
@@ -3,21 +3,48 @@ fetch("./db.json")
     .then((resp) => resp.json())
     .then((data) => renderCompanies(data))
 
+// GLOBAL SCOPE
+
 const animeCom = document.getElementById('animeCompanies')
 const centerImage = document.getElementById('studioImgDisplay')
+const centerName = document.getElementById('studioNameDisplay')
+const centerYear = document.getElementById('yearDisplay')
 
 
 // RENDERS
 
 function renderCompanies(studios) {
-    const studioCom = studios.company;
+
+    const studioCom = studios.company
+
     studioCom.forEach((studio) => {
-    const studioList = document.createElement('li')
-    studioList.textContent = studio.name;
-    animeCom.append(studioList)
-    studioList.addEventListener('click', (e) => {
-        centerImage.src = studio.image
-    })
+
+        const studioList = document.createElement('li')
+        studioList.textContent = studio.name
+        animeCom.append(studioList)
+        const animeDisp = document.getElementById('animeDisplay')
+        const animeLi = studio.anime
+
+        studioList.addEventListener('click', (e) => {
+
+            
+            centerImage.src = studio.image
+            centerName.textContent = studio.name
+            centerYear.textContent = studio.founded
+            
+            
+            animeLi.forEach((anime) => {
+                const animeMain = document.querySelectorAll('.animeThumbnail')
+                const animeImg = document.querySelectorAll('.animeThumbnail > img')
+                animeImg.src = anime.image
+
+                animeMain.replaceChild("", animeImg)
+
+                
+            })
+
+
+        })
     })
 }
 
@@ -31,4 +58,4 @@ console.log('hi')
 
 const music = document.querySelector('#music');
 
-music.volume = 0;
+// music.volume = 0;
